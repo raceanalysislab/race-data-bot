9bdc824028db02cb94ee5a8448b027f171d1a670
raceanalysislab <race.analysis.lab@gmail.com>
2026-03-09 20:14:21 +0900

    fetch_players.py を更新

--- scripts/fetch_players.py
+++ scripts/fetch_players.py
@@ -3,28 +3,24 @@ from datetime import datetime
 
 BASE = "https://www1.mbrace.or.jp/od2/K/"
 
-def fetch(url):
 
+def fetch(url):
     r = requests.get(
         url,
         headers={
             "User-Agent": "Mozilla/5.0",
         },
-        timeout=30
+        timeout=30,
     )
-
     r.raise_for_status()
-
     return r.text
 
 
 def main():
-
     today = datetime.now()
-
     yymmdd = today.strftime("%y%m%d")
 
-    url = f"{BASE}B{yymmdd}.TXT"
+    url = f"{BASE}b{yymmdd}.txt"
 
     print("fetch:", url)
 
